545fc5499164ed53d6bbeb4918d00c6236cc7d50
Josh Tanenbaum <Joshua.D.Tanenbaum.20@dartmouth.edu>
2020-05-16 03:57:34 -0400

    lab5 done

---
+++ src/router.js
@@ -0,0 +1,22 @@
+import { Router } from 'express';
+import * as Posts from './controllers/post_controller';
+
+
+const router = Router();
+
+router.get('/', (req, res) => {
+  res.json({ message: 'welcome to our blog api!' });
+});
+
+///your routes will go here
+router.route('/posts')
+  .post((req, res) => { Posts.createPost(req, res) })
+  .get((req, res) => { Posts.getPosts(req, res) });
+
+router.route('/posts/:id')
+  .get((req, res) => { Posts.getPost(req, res) })
+  .put((req, res) => { Posts.updatePost(req, res) })
+  .delete((req, res) => { Posts.deletePost(req, res) });
+
+
+export default router;
--- src/server.js
+++ src/server.js
@@ -1,8 +1,17 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
-import path from 'path';
+// import path from 'path';
 import morgan from 'morgan';
+import mongoose from 'mongoose';
+import apiRouter from './router';
+
+
+// DB Setup
+const mongoURI = process.env.MONGODB_URI || 'mongodb://localhost/blog';
+mongoose.connect(mongoURI);
+// set mongoose promises to es6 default
+mongoose.Promise = global.Promise;
 
 // initialize
 const app = express();
@@ -14,19 +23,20 @@ app.use(cors());
 app.use(morgan('dev'));
 
 // enable only if you want templating
-app.set('view engine', 'ejs');
+//app.set('view engine', 'ejs');
 
 // enable only if you want static assets from folder static
 app.use(express.static('static'));
 
 // this just allows us to render ejs from the ../app/views directory
-app.set('views', path.join(__dirname, '../src/views'));
+//app.set('views', path.join(__dirname, '../src/views'));
 
 // enable json message body for posting data to API
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 // additional init stuff should go before hitting the routing
+app.use('/api', apiRouter);   //import event handler functions from apiRouter
 
 // default index route
 app.get('/', (req, res) => {
